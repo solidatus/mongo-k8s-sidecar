1c18f7c09dcb0cb9c8ea34abf37789356aefa9ce
Max Dudzinski <max.dudzinski@solidatus.com>
2024-02-28 13:07:18 +0000

    Add echo of image name for debug purps

--- docker-build-and-publish-dev.sh
+++ docker-build-and-publish-dev.sh
@@ -2,6 +2,7 @@
 
 VERSION=$(npm pkg get version | tr -d '"')
 IMAGE_NAME=docker.solidatus.com/infrastructure-dev/mongo-k8s-sidecar:$VERSION
+echo $IMAGE_NAME
 
 docker build . --platform linux/amd64 -t $IMAGE_NAME
 
