53fa26339a04fe63e02242abd132425432f25eee
Max Dudzinski <max.dudzinski@solidatus.com>
2024-06-18 08:09:26 +0100

    0.8.0

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 var worker = require('./lib/worker');
 var log = require('./lib/log');
 
-log.log('Starting up mongo-k8s-sidecar @ 0.3.0');
+log.log('Starting up mongo-k8s-sidecar @ 0.8.0');
 
 worker.init(function(err) {
   if (err) {
