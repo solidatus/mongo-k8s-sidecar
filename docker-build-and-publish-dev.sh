9913097e3571d8510aae3b13ed10dccf80f8b768
Max Dudzinski <max.dudzinski@solidatus.com>
2024-06-18 15:17:44 +0100

    Update publish script to allow prod pushes

--- docker-build-and-publish-dev.sh
+++ docker-build-and-publish-dev.sh
@@ -2,8 +2,13 @@
 
 set -e
 
+# For dev just run like './docker-build-and-publish-dev.sh'
+# For prod run like './docker-build-and-publish-dev.sh infrastructure'
+
+PROJECT_NAME=${1:-infrastructure-dev}
+
 VERSION=$(npx npm pkg get version | tr -d '"')
-IMAGE_NAME=docker.solidatus.com/infrastructure-dev/mongo-k8s-sidecar:$VERSION
+IMAGE_NAME=docker.solidatus.com/$PROJECT_NAME/mongo-k8s-sidecar:$VERSION
 echo $IMAGE_NAME
 
 docker build . --platform linux/amd64 -t $IMAGE_NAME --progress=plain
