f4c7b1f5118aeca96abb8469726be10fd1bbe23c
Max Dudzinski <max.dudzinski@solidatus.com>
2025-01-13 13:41:47 +0000

    Fix README env prefix. Add 'KUBE_CLUSTER_SKIP_TLS_VERIFY' env var

--- src/k8s.ts
+++ src/k8s.ts
@@ -1,9 +1,20 @@
-import { CoreV1Api, KubeConfig, V1Pod } from "@kubernetes/client-node";
+import { CoreV1Api, KubeConfig } from "@kubernetes/client-node";
 
 import { config } from "./config";
 
+import type { Cluster, V1Pod } from "@kubernetes/client-node";
+
 const kc = new KubeConfig();
 kc.loadFromDefault();
+
+if (config.kube.clusterSkipTLSVerify) {
+  // Cluster.skipTLSVerify is readonly. Let's respect it and create new cluster objects.
+  // Assigning to KubeConfig.clusters appears to be the way, same as the 'loadFromCluster' in config.js of the k8s client lib.
+  kc.clusters = kc.clusters.map<Cluster>((cluster) => ({
+    ...cluster,
+    skipTLSVerify: true,
+  }));
+}
 
 const k8sApi = kc.makeApiClient(CoreV1Api);
 
